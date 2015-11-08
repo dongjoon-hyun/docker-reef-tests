7ca723c3f0672a33368b961a3a2e62248cb0130d
Dongjoon Hyun <dongjoon@apache.org>
2015-11-09 01:52:27 +0900

    Add HDP 2.2.8

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ docker build -t ubuntu12.04-jdk7-hdp2.1.15 ubuntu12.04-jdk7-hdp2.1.15
 docker tag -f ubuntu12.04-jdk7-hdp2.1.15 hdi3.1
 docker build -t ubuntu12.04-jdk7-hdp2.2.7 ubuntu12.04-jdk7-hdp2.2.7
 docker tag -f ubuntu12.04-jdk7-hdp2.2.7 hdi3.2
+docker build -t ubuntu12.04-jdk7-hdp2.2.8 ubuntu12.04-jdk7-hdp2.2.8
+docker tag -f ubuntu12.04-jdk7-hdp2.2.8 hdp2.2
---
+++ ubuntu12.04-jdk7-hdp2.2.8/init-nn.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+/usr/sbin/sshd
+
+/root/sync-hosts.sh
+
+grep hdn /etc/hosts | awk '{print $1}' | sort | uniq > $HADOOP_PREFIX/etc/hadoop/slaves
+for host in `grep 172 /etc/hosts | awk '{print $1}'`
+do
+    echo $host
+    scp $HADOOP_PREFIX/etc/hadoop/slaves $host:$HADOOP_PREFIX/etc/hadoop/slaves
+done
+
+hdfs namenode -format
+
+hadoop-daemon.sh --script hdfs start namenode
+slaves.sh /usr/hdp/2.2.8.0-3150/hadoop/sbin/hadoop-daemon.sh --script hdfs start datanode
+
+yarn-daemon.sh start resourcemanager
+slaves.sh /usr/hdp/2.2.8.0-3150/hadoop-yarn/sbin/yarn-daemon.sh start nodemanager
+
+cd ~ && /bin/bash
