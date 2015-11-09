801c9eeb24988e4254e090c4d072027f0e3e98da
Dongjoon Hyun <dongjoon@apache.org>
2015-11-10 04:30:05 +0900

    Change image name

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker build -t ubuntu12.04-jdk7-hdp2.2.8 ubuntu12.04-jdk7-hdp2.2.8
 docker tag -f ubuntu12.04-jdk7-hdp2.2.8 hdp2.2
 docker build -t ubuntu12.04-jdk7-hdp2.3.2 ubuntu12.04-jdk7-hdp2.3.2
 docker tag -f ubuntu12.04-jdk7-hdp2.3.2 hdp2.3
-docker build -t ubuntu12.04-jdk7-hadoop2.7.1 ubuntu12.04-jdk7-hadoop2.7.1
-docker tag -f ubuntu12.04-jdk7-hadoop2.7.1 apache
+docker build -t ubuntu12.04-jdk7-apache2.7.1 ubuntu12.04-jdk7-apache2.7.1
+docker tag -f ubuntu12.04-jdk7-apache2.7.1 apache
 docker build -t ubuntu12.04-jdk7-mesos0.25 ubuntu12.04-jdk7-mesos0.25
 docker tag -f ubuntu12.04-jdk7-mesos0.25 mesos0.25
