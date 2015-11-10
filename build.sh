f1b30556c6ce03bf7d448b57d29523bf6f8f7433
Dongjoon Hyun <dongjoon@apache.org>
2015-11-10 15:11:15 +0900

    Add Organization name to register DockerHub

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+ORG=reefrt
 for DIR in `ls -d ubuntu12.04*`
 do
     IMAGE=$DIR
     TAG=${DIR##*-}
-    echo $DIR $TAG
-    docker build -t $IMAGE $DIR
-    docker tag -f $IMAGE $TAG
+    echo $DIR $ORG/$TAG
+    docker build -t $ORG/$IMAGE $DIR
+    docker tag -f $ORG/$IMAGE $TAG
 done
-docker tag -f ubuntu12.04-jdk7-apache2.7.1 apache2.7
-docker tag -f ubuntu12.04-jdk7-hdp2.1.15 hdi3.1
-docker tag -f ubuntu12.04-jdk7-hdp2.2.7 hdi3.2
+docker tag -f $ORG/ubuntu12.04-jdk7-apache2.7.1 $ORG/apache2.7
+docker tag -f $ORG/ubuntu12.04-jdk7-hdp2.1.15 $ORG/hdi3.1
+docker tag -f $ORG/ubuntu12.04-jdk7-hdp2.2.7 $ORG/hdi3.2
